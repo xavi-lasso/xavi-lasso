10c9e47dc4586aaa6bbbee1d04b886b6d18f14e3
Xavi Lasso <xavi@xavi.works>
2023-10-10 17:23:48 -0500

    newLanding

--- scripts/main.js
+++ scripts/main.js
@@ -15,18 +15,27 @@
 //   }]
 // });
 
-barba.init({
-  transitions: [{
-    name: 'opacity-transition',
-    leave(data) {
-      return gsap.to(data.current.container, {
-        opacity: 0
-      });
-    },
-    enter(data) {
-      return gsap.from(data.next.container, {
-        opacity: 0
-      });
-    }
-  }]
-});
+setInterval(()=>{
+  const time = document.querySelector("#time");
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let day_night = "AM";
+  // if(hours > 12){
+  //     hours = hours - 12;
+  //     day_night = "PM";
+  // } 
+  if ( hours < 10){
+      hours = "0" + hours;
+  }
+  if (minutes < 10) {
+      minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+  seconds = "0" + seconds;
+  }
+  time.textContent = hours + ":" + minutes + ":" + seconds;
+ }
+
+ )
