129e591081f24aac96bc4a0bf695b5d2e7e6587d
Xavi Lasso <xavi@xavi.works>
2023-12-01 09:45:24 -0500

    new layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-//**** Time ****
+// TIME OF DAY
 
 setInterval(()=>{
   const time = document.querySelector("#time");
@@ -7,11 +7,11 @@ setInterval(()=>{
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  let day_night = "AM";
-  // if(hours > 12){
-  //     hours = hours - 12;
-  //     day_night = "PM";
-  // } 
+  let day_night = " AM";
+  if(hours > 12){
+      hours = hours - 12;
+      day_night = " PM";
+  } 
   if ( hours < 10){
       hours = "0" + hours;
   }
@@ -21,41 +21,7 @@ setInterval(()=>{
   if (seconds < 10) {
   seconds = "0" + seconds;
   }
-  time.textContent = hours + ":" + minutes + ":" + seconds;
+  time.textContent = hours + ":" + minutes + ":" + seconds + day_night;
  }
 
  )
-
-// **** Follow Mouse ****
- 
-     TweenMax.from(".cursor-follow", 2, {
-         delay: 1,
-         opacity: 0
-     });
-
-     follower = document.querySelector('.cursor-follow');
-
-     posX = 0;
-     posY = 0;
-     mouseX = 0;
-     mouseY = 0;
-
-     TweenMax.to({}, 0.016, {
-         repeat: -1,
-         onRepeat: function () {
-             posX += (mouseX - posX) / 9;
-             posY += (mouseY - posY) / 9;
-
-             TweenMax.set(follower, {
-                 css: {
-                     left: posX - 200,
-                     top: posY - 250,
-                 }
-             });
-         }
-     });
-
-     document.addEventListener("mousemove", function (e) {
-     mouseX = e.pageX;
-     mouseY = e.pageY;
- });
